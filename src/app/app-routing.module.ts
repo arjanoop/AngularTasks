3025ac3eb20b87f95af9211896bfc7788dd06a0c
Aj <Arjanoop@gmail.com>
2019-11-28 18:03:25 +0530

    Initial commit for task-1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { ContentComponent } from './content/content.component';
+import { ArticleComponent } from './article/article.component';
+import { CreateArticleComponent } from './create-article/create-article.component';
 
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+    { path:'', component : ContentComponent},
+    { path:'content', component : ContentComponent},
+    { path:'article', component : ArticleComponent},
+    { path:'create-article', component : CreateArticleComponent}
+  ];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+  @NgModule({
+      imports : [RouterModule.forRoot(appRoutes)],
+      exports : [RouterModule]
+  })
+
+  export class AppRoutingModule{
+
+  }
---
+++ src/app/format.ts
@@ -0,0 +1,9 @@
+export interface ArticleFormat {
+    title: string; 
+    date: string; 
+    content: string; 
+    category: string;
+    author: string;
+    image: string; 
+    value: number;
+}
